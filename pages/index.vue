870152f92cd2aef2ad25af48fb46f13775a7ab55
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-11-08 23:59:17 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        wikiarchives.space
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.title {
+  text-align: center;
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+</style>
