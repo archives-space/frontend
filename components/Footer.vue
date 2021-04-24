c9ed66ad9590d98f55821f691c934b043f4656fb
Matthieu Bessat <spamfree@matthieubessat.fr>
2021-04-24 12:28:26 +0200

    fix(Footer): container

--- components/Footer.vue
+++ components/Footer.vue
@@ -24,45 +24,41 @@
       </v-card-text>
 
       <v-card-text class="white--text pt-0">
-        <Container>
-          <v-row justify="center" no-gutters>
-            <v-col cols="12" sm="12" md="8">
-              <v-row no-gutters>
-                <v-col
-                  class="d-flex align-center"
-                  cols="12"
-                  sm="7"
-                >
-                  <p style="text-align:left; margin-bottom: 0;">
-                    WikiArchives.space is a collaborative photo gallery aiming to host every single public domain and CC-NC space photos ever taken, and distribute it to everyone for free.
+        <Container :md="11" :lg="9">
+          <v-row no-gutters>
+            <v-col
+              class="d-flex align-center"
+              cols="12"
+              sm="7"
+            >
+              <p style="text-align:left; margin-bottom: 0;">
+                WikiArchives.space is a collaborative photo gallery aiming to host every single public domain and CC-NC space photos ever taken, and distribute it to everyone for free.
 
-                    Each photo added is carefully and manually treated by an archivist, to make sure you get the date and location it was taken, an extensive description, and the source.
-                  </p>
-                </v-col>
+                Each photo added is carefully and manually treated by an archivist, to make sure you get the date and location it was taken, an extensive description, and the source.
+              </p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="5"
+            >
+              <v-row justify="center" class="mt-3 mt-md-0">
                 <v-col
                   cols="12"
-                  sm="5"
+                  lg="7"
+                  md="8"
+                  sm="11"
+                  class="d-flex flex-column"
                 >
-                  <v-row justify="center" class="mt-3 mt-md-0">
-                    <v-col
-                      cols="12"
-                      lg="7"
-                      md="8"
-                      sm="11"
-                      class="d-flex flex-column"
-                    >
-                      <v-btn
-                        v-for="link in links"
-                        :key="link.id"
-                        color="white"
-                        text
-                        rounded
-                        @click="openLink(link)"
-                      >
-                        {{ link.name }}
-                      </v-btn>
-                    </v-col>
-                  </v-row>
+                  <v-btn
+                    v-for="link in links"
+                    :key="link.id"
+                    color="white"
+                    text
+                    rounded
+                    @click="openLink(link)"
+                  >
+                    {{ link.name }}
+                  </v-btn>
                 </v-col>
               </v-row>
             </v-col>
