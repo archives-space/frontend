27249352146a46ec38a1396690b2a9c9c3e0d26a
Matthieu Bessat <spamfree@matthieubessat.fr>
2021-04-24 12:27:54 +0200

    fix: global loading overflow

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,7 +1,15 @@
 <template>
   <v-fade-transition>
-    <div v-if="loading" class="loading-global">
-      <v-progress-circular indeterminate size="50" style="color:white" />
+    <div
+      v-if="loading"
+      class="loading-global"
+    >
+      <v-progress-circular
+        indeterminate
+        size="50"
+        class="mb-2"
+        style="color:white"
+      />
     </div>
   </v-fade-transition>
 </template>
@@ -15,24 +23,31 @@ export default {
   methods: {
     start () {
       this.hold = true
+      // debouncing
       setTimeout(() => {
         if (this.hold) {
+          // enable the actual loader
+          document.body.setAttribute('class', document.body.className + ' body-no-overflow')
           this.loading = true
           this.hold = false
         }
-      }, 100) // debouncing
+      }, 100)
     },
     finish () {
       this.loading = false
       this.hold = false
+      document.body.setAttribute('class', document.body.className.replace(' body-no-overflow', ''))
     }
   }
 }
 </script>
 
-<style scoped>
+<style>
+  .body-no-overflow {
+    overflow: hidden;
+  }
   .loading-global{
-    position: absolute;
+    position: fixed;
     top: 0;
     left: 0;
     z-index: 9999;
