3e408e647bb8f263ebe68b6e5410b0a062fc6d24
Matthieu Bessat <spamfree@matthieubessat.fr>
2021-04-24 12:21:17 +0200

    fix: add title to error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="mt-5 container d-flex justify-center text-center">
+  <div class="my-5 container d-flex justify-center text-center">
     <div>
       <h1 class="text-h3 mt-4">
         We're sorry :(
       </h1>
-      <h3 class="text-h1 mt-4">{{ error.statusCode }}</h3>
+      <h3 class="text-h1 mt-4">
+        {{ error.statusCode }}
+      </h3>
       <div class="mt-3">
-        <NuxtLink to="/">Home page</NuxtLink>
+        <NuxtLink to="/">
+          Home page
+        </NuxtLink>
       </div>
     </div>
   </div>
@@ -14,7 +18,19 @@
 
 <script>
 export default {
-  props: ['error'],
-  layout: 'blog' // you can set a custom layout for the error page
+  layout: 'default',
+  props: {
+    error: {
+      type: Object,
+      default: () => ({
+        statusCode: 500
+      })
+    }
+  },
+  head () {
+    return {
+      title: this.error.statusCode
+    }
+  }
 }
 </script>
